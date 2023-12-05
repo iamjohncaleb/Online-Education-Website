9ee36ebef8fe9ac337ae22b4c150d57387833245
Caleb John <johncaleb022@gmail.com>
2023-12-05 00:35:24 -0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -29,6 +29,7 @@ document.querySelector('.side-bar .close-side-bar').onclick = () =>{
    body.classList.remove('active');
 }
 
+
 document.querySelectorAll('input[type="number"]').forEach(InputNumber => {
    InputNumber.oninput = () =>{
       if(InputNumber.value.length > InputNumber.maxLength) InputNumber.value = InputNumber.value.slice(0, InputNumber.maxLength);
