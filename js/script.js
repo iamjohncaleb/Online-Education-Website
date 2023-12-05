90eb3f2aedd4d0b30cef3a37d85155b6506ee949
Caleb John <johncaleb022@gmail.com>
2023-12-05 00:34:16 -0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -23,10 +23,12 @@ document.querySelector('#menu-btn').onclick = () =>{
    body.classList.toggle('active');
 }
 
+
 document.querySelector('.side-bar .close-side-bar').onclick = () =>{
    sideBar.classList.remove('active');
    body.classList.remove('active');
 }
+
 
 document.querySelectorAll('input[type="number"]').forEach(InputNumber => {
    InputNumber.oninput = () =>{
